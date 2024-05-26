d7e0954a7e8fe932b9daf6af38c2811f9a2dc038
{JetBrain} <108869575+JetBrain2056@users.noreply.github.com>
2024-05-26 07:32:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express            = require('express');
+const { router }         = require('./src/router.js');
 
 const port   = process.env.PORT||9000;
 const host   = process.env.HOST||'0.0.0.0'||'127.0.0.1';
@@ -9,12 +10,8 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 
 server.use("/@tensorflow"       , express.static('./node_modules/@tensorflow')); 
-
-//assuming app is express Object.
-server.get('/',function(req,res) {
-    res.render('index.html');
-  });  
+server.use(router);  
 
 server.listen(port, host, () => {
-  console.log(`Server is running on http://${host}:${port}`);
+  console.log(`Server is running on http://${host}:${port}`);  
 });
