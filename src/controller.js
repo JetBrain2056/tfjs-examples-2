eed6bbce2a03b145508244116e78bfceabc5bca1
{JetBrain} <108869575+JetBrain2056@users.noreply.github.com>
2024-11-16 10:59:50 +0300

    Update controller.js

--- src/controller.js
+++ src/controller.js
@@ -2,7 +2,7 @@ const fs   = require('fs');
 // const path = require('path');
 // const tf   = require("@tensorflow/tfjs");
 const tf = require('@tensorflow/tfjs-node');
-require('@tensorflow/tfjs-node-gpu');
+//const tf = require('@tensorflow/tfjs-node-gpu');
 // const URL   = 'http://localhost:9000/app/model/model.json';
 const URL = 'https://raw.githubusercontent.com/MikeShi42/yolo-tiny-tfjs/master/model2.json';
 
@@ -13,7 +13,8 @@ function dateNow() {
     return date.toISOString().slice(0, 19).replace('T', ' ');
 }
 function trainModel() {
-        //chatGPT version////    
+
+    //chatGPT version////    
 
     function yoloLayers() {
         //const anchors = [1.08, 1.19, 3.42, 4.41, 6.63, 11.38, 9.42, 5.11, 16.62, 10.52];
@@ -35,7 +36,7 @@ function trainModel() {
         model.add(tf.layers.maxPooling2d({ poolSize: [2, 2], strides: [1, 1], padding: 'same' }));
         model.add(tf.layers.conv2d({ filters: 1024, kernelSize: 3, strides: 1, padding: 'same', activation: 'LeakyReLU' }));
         model.add(tf.layers.conv2d({ filters: 1024, kernelSize: 3, strides: 1, padding: 'same', activation: 'LeakyReLU' }));
-        model.add(tf.layers.conv2d({ filters: 425, kernelSize: 1, strides: 1, padding: 'same' }));
+        model.add(tf.layers.conv2d({ filters: 75, kernelSize: 1, strides: 1, padding: 'same' }));
 
         return model;
     }
@@ -50,6 +51,7 @@ function trainModel() {
     model.summary();
 
     return model;
+
 }
 exports.app = async function(req, res) {
     if (!req.body) return res.sendStatus(400);
@@ -57,32 +59,20 @@ exports.app = async function(req, res) {
 }
 exports.tfTrain = async function(req,res) {
     console.log(dateNow(),'>>tfTrain()...');
-    if (!req.body) return res.sendStatus(400);
 
-    // const {} = req.body;
+    if (!req.body) return res.sendStatus(400);
 
     model = await tf.loadLayersModel(URL);  
     model.summary();
      
     //model.predict(tf.zeros([1, 416, 416, 3]));
     
-    CLASSES = [];
-    CLASSES.push('Class1');
-    CLASSES.push('Class2');
-    CLASSES.push('Class3');
-    
-    let epochs, optimizer, inputsAsTensor, targetTensor;
+    let inputsAsTensor, targetTensor;
 
-    // if (tf.getBackend()==='cpu') {
-    //     tf.setBackend('gpu');
+    // console.log('BackEnd:',tf.getBackend());
+    // if (tf.getBackend()==='cpu') {        
+    //    tf.setBackend('gpu');
     // }
-
-    // Freeze the layers of the pre-trained model except for the last few
-    for (let i = 0; i < model.layers.length; i++) {
-        model.layers[i].trainable = false;
-    } 
-
-    model.layers[model.layers.length - 1].trainable = true; // Разморозка последнего слоя для дообучения
 
     let trainingDataInputs  = [];
     let trainingDataOutputs = [];
@@ -107,9 +97,9 @@ exports.tfTrain = async function(req,res) {
     img[4] = {class:1, xmin:221, ymin:90, xmax:256, ymax:187, name:'train416.jpg'}
     img[5] = {class:2, xmin:257, ymin:81, xmax:289, ymax:170, name:'train416.jpg'}
     img[6] = {class:2, xmin:295, ymin:82, xmax:330, ymax:180, name:'train416.jpg'}
-    for (i=7;i<80;i++) {
-        img.push({class:i-4, xmin:0, ymin:0, xmax:416, ymax:416, name:'train416.jpg'});
-    }
+    // for (i=7;i<80;i++) {
+    //     img.push({class:i-4, xmin:0, ymin:0, xmax:1, ymax:1, name:'train2.jpg'});
+    // }
 
     for (let i of img) {
         const trainImage = fs.readFileSync('./app/images/'+i.name); 
@@ -122,15 +112,13 @@ exports.tfTrain = async function(req,res) {
     
     inputsAsTensor  = await tf.concat(trainingDataInputs);
     
-    const ANCHORS = [0.573, 0.677, 1.87, 2.06, 3.34, 5.47, 7.88, 3.53, 9.77, 9.17]; 
-    function _alogistic(e) {      
-    
-        // return Math.log(1/e-1)
-        return Math.log(e)
-    }
+    //v2 yolo_tiny
+    const ANCHORS = [0.573, 0.677, 1.87, 2.06, 3.34, 5.47, 7.88, 3.53, 9.77, 9.17];   
+    //const ANCHORS = [1.08, 1.19, 3.42, 4.41, 6.63, 11.38, 9.42, 5.11, 16.62, 10.52];
 
-    // inputsAsTensor = tf.zeros([1,416,416,3]);  
-    zerosTensor = tf.zeros([1,13,13,425]);  
+    //inputsAsTensor = tf.zeros([1,416,416,3]);  
+    //zerosTensor = tf.zeros([1,13,13,425]);  
+    zerosTensor = tf.zeros([1,13,13,75]);  
 
     for (let c of img) {
         
@@ -142,11 +130,6 @@ exports.tfTrain = async function(req,res) {
         const y1 = c.ymin/416;
         const x2 = c.xmax/416;
         const y2 = c.ymax/416;
-
-        // console.log(x1)
-        // console.log(y1)
-        // console.log(x2)
-        // console.log(y2)
 
         bx = x1 + (x2-x1)/2
         by = y1 + (y2-y1)/2
@@ -160,60 +143,59 @@ exports.tfTrain = async function(req,res) {
 
         let offset = 0;
         for (xg = 0; xg<13; xg++) {
-        for (yg = 0; yg<13; yg++) {        
-            for (p = 0; p<5; p++) {
-            //The center object of the trained image
-            if (xg===Math.round(bx*13)&&yg===Math.round(by*13)&&p===Math.round(bw*5/2+bh*5/2)) { 
-                // Transfer to 0~1 corresponding to each grid cell:
-                // pixl[offset++] = _alogistic(bx*13) - xg
-                // pixl[offset++] = _alogistic(by*13) - yg
-                pixl[offset++] = bx*13 - xg
-                pixl[offset++] = by*13 - yg
-                pixl[offset++] = Math.log(bw*13/ANCHORS[p * 2])
-                pixl[offset++] = Math.log(bh*13/ANCHORS[p * 2 + 1])              
-                pixl[offset++] = 1            
-                pixl[offset+c.class] = 1
-                
-            } else {       
-                offset += 5;
+            for (yg = 0; yg<13; yg++) {        
+                for (p = 0; p<5; p++) {
+                //The center object of the trained image
+                if (xg===Math.round(bx*13)&&yg===Math.round(by*13)&&p===Math.round(bw*5/2+bh*5/2)) { 
+                    // Transfer to 0~1 corresponding to each grid cell:                                    
+                    pixl[offset++] = bx*13 - xg
+                    pixl[offset++] = by*13 - yg
+                    pixl[offset++] = Math.log(bw*13/ANCHORS[p * 2])
+                    pixl[offset++] = Math.log(bh*13/ANCHORS[p * 2 + 1])                          
+                    pixl[offset++] = 1            
+                    pixl[offset+c.class] = 1                                                            
+                                        
+                } else {       
+                    offset += 5;
+                }
+                offset += 3;
+                }
             }
-            offset += 80;
-            }
-        }
         }
 
-        tfpixl = tf.tensor(pixl, [1,13,13,425], 'float32');
-        // console.log(await tfpixl.array())
+        //tfpixl = tf.tensor(pixl, [1,13,13,425], 'float32');
+        tfpixl = tf.tensor(pixl, [1,13,13,75], 'float32');
 
         trainingDataOutputs.push(tfpixl);
     }
         
     targetTensor    = await tf.concat(trainingDataOutputs);  
-    // console.log(targetTensor.dataSync())
 
-    // model.summary();
+    // Freeze the layers of the pre-trained model except for the last few
+    for (let i = 0; i < model.layers.length; i++) {
+        model.layers[i].trainable = false;
+    } 
+           
+    model.layers[model.layers.length - 1].trainable = true; 
+        
+    console.log(model.layers[model.layers.length - 1]);    
 
-    optimizer = tf.train.adam();
-    model.compile({
-        optimizer: optimizer,
-        loss: 'binaryCrossentropy',
-        // loss: 'categoricalCrossentropy',
+    const L1L2 = { l1: 0, l2: 0.0005000000237487257, hasL1: false, hasL2: true }
+    const layer = tf.layers.conv2d({ filters: 75, rank:2, kernelSize: [1,1], strides: [1,1], padding: 'same'});
+    const newModel = tf.model({inputs: model.inputs, outputs: layer.apply(model.output)});
+
+    newModel.compile({
+        optimizer:  'adam',
+        //loss: 'meanSquaredError',
+        loss: 'binaryCrossentropy',        
         metrics: ['accuracy'],
-    });
-
-    // console.log(inputsAsTensor);
-    // console.log(targetTensor); 
-    
-    epochs = 5;  
-    await model.fit(inputsAsTensor, targetTensor, {    
-        shuffle   : true, 
-        batchSize : 32,     
-        // validationSplitRetrain : 0.99,
-        epochs    : epochs, 
-        // callbacks : { onEpochEnd: async (epoch,logs) => {
-        // progress.value = epoch/(epochs-1)*100;
-        // console.log('Epoch', epoch, logs)
-        // }}
+    });    
+      
+    await newModel.fit(inputsAsTensor, targetTensor, {
+        epochs: 10,
+        shuffle: true,
+        validationSplit: 0.2,
+        batchSize : 32, 
     });
 
     inputsAsTensor.dispose();
@@ -221,9 +203,9 @@ exports.tfTrain = async function(req,res) {
 
     // await model.save('./app/model/newmodel');   
     
-    const predictImage = fs.readFileSync('./app/images/train.jpg'); 
+    const predictImage = fs.readFileSync('./app/images/train416.jpg'); 
     imageFeatures = await calculateImageFeatures(predictImage); 
-    outputs = model.predict(imageFeatures);
+    outputs = newModel.predict(imageFeatures);
     predictions = await outputs.array();
 
     try {        
